b0a6b32e07cd9b8b1a7401708439b328187114df
Lujunhao <lujunhao@baidu.com>
2018-07-13 16:10:46 +0800

    add out put

--- release.sh
+++
@@ -1,9 +0,0 @@
-echo "execute webpack packing......"
-rm -rf output
-npm run build
-echo "webpack done ! "
-tar -zcvf output.tar.gz ./output
-rm -rf output
-mkdir output
-mv output.tar.gz ./output
-echo "build done ! "
